0542cd39d3aa075721600451320c316740954962
Gabriel <gabrielfoganholi@hotmail.com>
2026-06-01 05:31:03 -0300

    Melhora navegacao, busca e experiencia mobile

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,13 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import PackageDetails from "./pages/PackageDetails";
 import Schedule from "./pages/Schedule";
+import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/pacote/:slug" element={<PackageDetails />} />
